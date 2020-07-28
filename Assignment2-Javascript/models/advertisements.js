7c3ea2cae252ef019e92c7b2bd7950352552ae93
Manjot Singh Saha <200427314@student.georgianc.on.ca>
2020-07-27 23:47:01 -0400

    Add project files.

---
+++ Assignment2-Javascript/models/advertisements.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+//Create Schema
+const AdSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        trim: true
+    }
+});
+
+//Create and instantiate model with schema
+const Advertisements = mongoose.model("Advertisements", AdSchema);
+module.exports = Advertisements;
