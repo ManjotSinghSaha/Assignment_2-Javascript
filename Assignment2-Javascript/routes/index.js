bd35c741949332e182a26d857cb762bc5e047f8a
Manjot Singh Saha <200427314@student.georgianc.on.ca>
2020-07-27 23:50:51 -0400

    commit 4

--- Assignment2-Javascript/routes/index.js
+++ Assignment2-Javascript/routes/index.js
@@ -1,10 +1,21 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var adsModel = require('../models/advertisements');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    try {
+        adsModel.find({}, function (err, foundAds) {
+            console.log(err);
+            console.log(foundAds);
+            res.render('index', { advertisements: foundAds });
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.render('index', { title: 'Express' });
+    }
 });
 
 module.exports = router;
