5330b2a42dedc92eaa39ae1c88072fb1d12a2975
Manjot Singh Saha <200427314@student.georgianc.on.ca>
2020-07-31 16:47:22 -0400

    commit 5

--- Assignment2-Javascript/routes/index.js
+++ Assignment2-Javascript/routes/index.js
@@ -7,15 +7,66 @@ var adsModel = require('../models/advertisements');
 router.get('/', function (req, res) {
     try {
         adsModel.find({}, function (err, foundAds) {
-            console.log(err);
-            console.log(foundAds);
-            res.render('index', { advertisements: foundAds });
+            if (!err)
+            res.render('index', { advertisements : foundAds });
         });
     }
     catch (err) {
-        console.log(err);
         res.render('index', { title: 'Express' });
     }
+});
+
+/* GET for insert page */
+router.get('/insert', function (req, res) {
+    res.render('insert');
+});
+
+/* POST for insert page */
+router.post('/insert', function (req, res) {
+    const ad = new adsModel({
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+    });
+    ad.save(function (err) {
+        if (!err)
+            res.redirect('/');
+        else
+            console.log(err);
+    });
+});
+
+/* GET for Update page */
+router.get('/update/:id', function (req, res) {
+    adsModel.findById(req.params.id, function (err, foundAds) {
+        if (err)
+            console.log(err);
+        else
+            res.render('update', { ad: foundAds });
+    });
+});
+
+router.post('/update', function (req, res) {
+    adsModel.findByIdAndUpdate(req.params.id,
+    {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+    }), function (err) {
+        if (err)
+            console.log(err);
+        else
+            res.redirect('/');
+    }
+});
+
+router.post('/delete/:id', function (req, res) {
+    adsModel.findByIdAndDelete(req.params.id, function (err) {
+        if (!err)
+            res.redirect('/');
+        else
+            console.log(err);
+    })
 });
 
 module.exports = router;
