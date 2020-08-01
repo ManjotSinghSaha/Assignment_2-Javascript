eca74cc0898121a55b2cd393b99ce110d1079195
Manjot Singh Saha <200427314@student.georgianc.on.ca>
2020-08-01 01:15:02 -0400

    file upload and css

--- Assignment2-Javascript/models/advertisements.js
+++ Assignment2-Javascript/models/advertisements.js
@@ -7,6 +7,9 @@ const AdSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
+    image: {
+        type: String
+    },
     description: {
         type: String,
         required: true,
--- Assignment2-Javascript/routes/index.js
+++ Assignment2-Javascript/routes/index.js
@@ -2,37 +2,88 @@
 var express = require('express');
 var router = express.Router();
 var adsModel = require('../models/advertisements');
+var formidable = require('formidable');
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res) {
     try {
         adsModel.find({}, function (err, foundAds) {
             if (!err)
-            res.render('index', { advertisements : foundAds });
+                res.render('index', { title: 'Home', advertisements: foundAds });
         });
     }
     catch (err) {
-        res.render('index', { title: 'Express' });
+        res.render('index', { title: 'Home' });
     }
 });
 
+/* GET About Me page */
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'About Me' });
+});
+
+/* GET contact page. */
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact' });
+});
+
+/* GET projects page. */
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+    /* CRUD Operations
+///////////////////////////////////////////////////////////////////////////////
+
+
+
 /* GET for Insert page */
 router.get('/insert', function (req, res) {
-    res.render('insert');
+    res.render('insert', {title  : 'Insert'});
 });
 
 /* POST for Insert page */
 router.post('/insert', function (req, res) {
-    const ad = new adsModel({
-        title: req.body.title,
-        description: req.body.description,
-        price: req.body.price
+    var form = new formidable.IncomingForm();
+
+    //Specify our image file directory
+    form.uploadDir = path.join(__dirname, '../public/images');
+
+    form.parse(req, function (err, fields, files) {
+        //Update filename
+        files.upload.name = fields.pictitle + '.' + files.upload.name.split('.')[1];
+
+        // Insert ads to the database
+        const ad = new adsModel({
+            title: fields.title,
+            image: files.upload.name,
+            description: fields.description,
+            price: fields.price
+        });
+        ad.save(function (err) {
+            if (err)
+                console.log(err);
+        });
+
+        //Upload file on our server
+        fs.rename(files.upload.path, path.join(form.uploadDir, files.upload.name), function (err) {
+            if (err)
+                console.log(err);
+        });
     });
-    ad.save(function (err) {
-        if (!err)
-            res.redirect('/');
-        else
-            console.log(err);
+    // This will redirect to home page after insertion
+    form.on('end', function (err, fields, files) {
+        res.redirect('/');
     });
 });
 
